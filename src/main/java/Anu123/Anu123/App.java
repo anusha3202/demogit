6ba1f392c133052f78f498be8695edd82ff195c9
anusha3202 <anushavigrahala3202@gmail.com>
2022-12-24 11:53:07 +0530

    my frist commit

---
+++ src/main/java/Anu123/Anu123/App.java
@@ -0,0 +1,13 @@
+package Anu123.Anu123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Sample123/test123.java
@@ -0,0 +1,5 @@
+package Sample123;
+
+public class test123 {
+
+}
